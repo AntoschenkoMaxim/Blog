eb76d83f3da98cd1433a6e4e6474e230766d3591
Maxim <jurusski@gmail.com>
2022-12-02 03:22:44 +0300

    Creating PostController
    PostController has been created with next functions: createPost and getPosts.

---
+++ server/controllers/post-controller.js
@@ -0,0 +1,47 @@
+const postService = require('../service/post-service')
+const { validationResult } = require('express-validator') //getting the result of validation
+const ApiError = require('../exceptions/api-error')
+
+class PostController {
+	async createPost(req, res, next) {
+		try {
+			const errors = validationResult(req)
+			if (!errors.isEmpty()) {
+				return ApiError.BadRequestError('Validation error', errors.array())
+			}
+			const { title, description, location } = req.body
+
+			const postData = await postService.createPost(title, description, location) //comments, createdBy, image, date
+			return res.json(postData)
+		} catch (error) {
+			next(error)
+		}
+	}
+
+	async getPosts(req, res, next) {
+		try {
+			const posts = await postService.getAllPosts() //getting posts by calling the getAllPosts function
+			return res.json(posts) //return json
+		} catch (error) {
+			next(error)//calling middleware
+		}
+	}
+
+	// async updatePost(req, res, next) {
+	// 	try {
+
+	// 	} catch (error) {
+	// 		next(error)
+	// 	}
+	// }
+
+	// async deletePost(req, res, next) {
+	// 	try {
+
+	// 	} catch (error) {
+	// 		next(error)
+	// 	}
+	// }
+}
+
+module.exports = new PostController()
