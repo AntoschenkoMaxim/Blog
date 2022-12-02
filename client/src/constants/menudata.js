34a06071d6ffec5a356fc93f4d61c96c643abbc4
Maxim <jurusski@gmail.com>
2022-12-02 14:39:28 +0300

    Creatig constants folder
    Constants folder has been created with faq data and menu data.

---
+++ client/src/constants/menudata.js
@@ -0,0 +1,26 @@
+import {
+	IconFileLike,
+	IconFileDownload,
+	IconFiles,
+	IconSettings,
+	IconQuestionMark,
+	IconUser,
+} from '@tabler/icons';
+
+const menudata = {
+	account: [
+		{ link: 'posts', label: 'Posts', icon: IconFiles },
+		{ link: 'likes', label: 'Likes', icon: IconFileLike },
+		{ link: 'saved', label: 'Saved', icon: IconFileDownload },
+		{ link: 'profile', label: 'Profile', icon: IconUser },
+		{ link: 'faq', label: 'FAQ', icon: IconQuestionMark },
+		{ link: 'settings', label: 'Settings', icon: IconSettings },
+	],
+	general: [
+		{ link: 'help', label: 'Posts', icon: IconSettings },
+		{ link: 'change', label: 'Posts', icon: IconSettings },
+		{ link: 'repost', label: 'Posts', icon: IconSettings },
+	],
+};
+
+export default menudata
