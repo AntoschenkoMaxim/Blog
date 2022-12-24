44aea01f2873aec7b56838bd20b500361858f0e2
Maxim <jurusski@gmail.com>
2022-12-24 13:51:40 +0300

    Changing validation rules
    Validation rules has been changed.

--- server/router/index.js
+++ server/router/index.js
@@ -6,17 +6,22 @@ const { body } = require('express-validator') //validation of the request body
 const authMiddleware = require('../middlewares/auth-middleware')
 
 router.post('/registration',
-	body('email').isEmail(), //validation of the email field
-	body('password').isLength({ min: 3, max: 32 }),
+	body('*.email').isEmail(), //validation of the email field
+	body('*.password').isLength({ min: 3, max: 32 }),
 	userController.registration
 )
-router.post('/login', userController.login)
+// * needed for object validation
+router.post('/login',
+	body('*.email').isEmail(),
+	body('*.password').isLength({ min: 3, max: 32 }),
+	userController.login)
 router.post('/logout', userController.logout)
 router.get('/activate/:link', userController.activate)
 router.get('/refresh', userController.refresh)
 router.get('/users', authMiddleware, userController.getUsers) //check if the user is logged in
 
 router.post('/create', authMiddleware, postController.createPost) //check if the user is logged in 
-router.get('/posts', authMiddleware, postController.getPosts)
+router.get('/posts', postController.getPosts)
+router.get('/delete/:id', postController.deletePostByID)
 
 module.exports = router
