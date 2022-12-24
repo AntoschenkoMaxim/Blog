2165793ff731e196f46894391bc0b3a9d7d157ea
Maxim <jurusski@gmail.com>
2022-12-24 13:05:25 +0300

    Creating export file for constants
    Export file for constants has been created.

---
+++ client/src/constants/index.js
@@ -0,0 +1,7 @@
+import faqData from './faqData'
+import menuData from './menuData'
+import requirementsData from './requirementsData'
+import validationData from './validationData'
+import selectSearchData from './selectSearchData'
+
+export { faqData, menuData, requirementsData, validationData, selectSearchData }
