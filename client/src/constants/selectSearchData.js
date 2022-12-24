74373426c6697165ab2cdd22e623b0d305f5e436
Maxim <jurusski@gmail.com>
2022-12-24 13:02:21 +0300

    Creating data for select
    Data for select has been created.

---
+++ client/src/constants/selectSearchData.js
@@ -0,0 +1,8 @@
+const selectSearchData = [
+  { value: 'likes', label: 'Likes' },
+  { value: 'title', label: 'Title' },
+  { value: 'description', label: 'Description' },
+  { value: 'comments', label: 'Comments' },
+]
+
+export default selectSearchData
