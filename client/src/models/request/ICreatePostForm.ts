8691f85702b0d3b627950491b19e964f034380d7
Maxim <jurusski@gmail.com>
2022-12-24 13:11:07 +0300

    Creating request folder with interfaces
    Request folder with interfaces has been created.

---
+++ client/src/models/request/ICreatePostForm.ts
@@ -0,0 +1,11 @@
+export interface ICreatePostForm {
+  title: string
+  description: string
+  location: string
+  // author: {
+  // 	id: string;
+  // 	email: string;
+  // 	firstName: string;
+  // 	lastName: string;
+  // }
+}
---
+++ client/src/models/request/IRegistrationForm.ts
@@ -0,0 +1,8 @@
+export interface IRegistrationForm {
+  email: string
+  password: string
+  firstName: string
+  lastName: string
+  location: string
+  // image: string;
+}
